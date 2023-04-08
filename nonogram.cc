b98aaf214fa3aa34582ec69c680d9ea469c3bb20
jaldridge <89814068+jaldridg@users.noreply.github.com>
2023-04-07 21:03:29 -0400

    Hello world!

---
+++ nonogram.cc
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "nonogram.hh"
+
+int main() {
+    test t = 1;
+    printf("%d\n", t);
+    return 0;
+}
---
+++ nonogram.hh
@@ -0,0 +1,6 @@
+#ifndef nonogram_hh
+#define nonogram_hh
+
+typedef int test;
+
+#endif
