71ebe236f528fd15a515cba1c2cfdbfd4cd84f3b
jaldridge <89814068+jaldridg@users.noreply.github.com>
2024-01-06 16:13:00 -0500

    Successful clue reading

--- board_reader.cc
+++ board_reader.cc
@@ -2,8 +2,10 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 BoardReader::BoardReader() {
+    // Read file
     clues_file = fopen("clues.txt", "r");
     if (!clues_file) {
         printf("File opening failed!");
@@ -14,9 +16,91 @@ BoardReader::BoardReader() {
     char * buffer = new char[length];
 
     rewind(clues_file);
-    fread(buffer, length, sizeof(char), clues_file);
+    fread(buffer, sizeof(char), length, clues_file);
+    fclose(clues_file);
 
     int i = 0;
-    while (buffer[i] != '.') { printf("%c - %d\n", buffer[i], buffer[i++]); }
-    fclose(clues_file);
+    int j = 0;
+    std::vector<int> clues = {};
+    char * clue_buffer = new char[3];
+    // Parse columns
+    while (true) {
+        // Stop: end of columns
+        if (buffer[i] == 10) {
+            clues.push_back(atoi(clue_buffer));
+            col_clues.push_back(clues);
+            j = 0;
+            break; 
+
+        // End of column
+        } else if (buffer[i] == ' ') {
+            clues.push_back(atoi(clue_buffer));
+            col_clues.push_back(clues);
+            clues = {};
+            j = 0;
+
+        // End of clue
+        } else if (buffer[i] == ',') {
+            clues.push_back(atoi(clue_buffer));
+            j = 0;
+
+        // Must be a number
+        } else {
+            clue_buffer[j] = buffer[i];
+            j++;
+        }
+        i++;
+    }
+
+    // Parse row clues
+    clues = {};
+    clue_buffer[0] = '\0';
+    clue_buffer[1] = '\0';
+    clue_buffer[2] = '\0';
+    while (true) {
+        // Stop: end of rows
+        if (buffer[i] == '.') {
+            clues.push_back(atoi(clue_buffer));
+            row_clues.push_back(clues);
+            break; 
+
+        // End of row
+        } else if (buffer[i] == 10) {
+            clues.push_back(atoi(clue_buffer));
+            row_clues.push_back(clues);
+            clues = {};
+            j = 0;
+
+        // End of clue
+        } else if (buffer[i] == ',') {
+            clues.push_back(atoi(clue_buffer));
+            j = 0;
+
+        // Must be a number
+        } else {
+            clue_buffer[j] = buffer[i];
+            j++;
+        }
+        i++;
+    }
+
+    
+    printf("clue size: %d\n", col_clues.size());
+    printf("clues:\n");
+    for (int i = 0; i < col_clues.size(); i++) {
+        for (int j = 0; j < col_clues.at(i).size(); j++) {
+            printf("%d ", col_clues.at(i).at(j));
+        }
+        printf("\n");
+    }
+
+    
+    printf("clue size: %d\n", row_clues.size());
+    printf("clues:\n");
+    for (int i = 0; i < row_clues.size(); i++) {
+        for (int j = 0; j < row_clues.at(i).size(); j++) {
+            printf("%d ", row_clues.at(i).at(j));
+        }
+        printf("\n");
+    }
 }
--- board_reader.hh
+++ board_reader.hh
@@ -2,6 +2,9 @@
 #define BOARD_READER_HH
 
 #include <stdio.h>
+#include <vector>
+
+typedef std::vector<std::vector<int>> clues;
 
 class BoardReader {
 public:
@@ -9,6 +12,9 @@ public:
 
 private:
     FILE * clues_file;
+
+    clues col_clues;
+    clues row_clues;
 };
 
 #endif
